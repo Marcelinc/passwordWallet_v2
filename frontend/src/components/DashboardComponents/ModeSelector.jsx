f80cbb9ca81f6f1ac75af75eae7c1801572de89a
Marcelinc <marcin.c40@wp.pl>
2024-08-17 16:51:38 +0200

    Fixed mode status display

--- frontend/src/components/DashboardComponents/ModeSelector.jsx
+++ frontend/src/components/DashboardComponents/ModeSelector.jsx
@@ -4,7 +4,7 @@ const ModeSelector = ({mode,onClickHandler}) => {
   return (
     <div className="modeSelector-container" onClick={onClickHandler} title={mode + ' Mode'}>
         <input type='checkbox' className='mode-input' checked={mode === 'Edit'} readOnly/>
-        <span className='mode-text'>{mode === 'Edit' ? 'Read Mode' : 'Manage Mode'}</span>
+        <span className='mode-text'>{mode === 'Edit' ? 'Manage Mode' : 'Read Mode'}</span>
     </div>
   )
 }
